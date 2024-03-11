cbeb299a01abdb2b9a0ca268ffca521554341f78
tanpaterusan <amaliatanpaterusan@gmail.com>
2024-03-11 18:09:05 +0700

    moving code

---
+++ purple/js/scripts.js
@@ -0,0 +1,14 @@
+/*!
+* Start Bootstrap - Full Width Pics v5.0.6 (https://startbootstrap.com/template/full-width-pics)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+// console.log('Window Location:', window.location);
+const too = window.location.search;
+const urlParams = new URLSearchParams(too);
+const param = urlParams.get('to');
+const upTo = param[0].toUpperCase() + param.substring(1);;
+// const upTo = param.capitalizeFirstLetter();
+document.getElementById('to').innerHTML = upTo +'  <i class="fa-solid fa-heart"></i>';
