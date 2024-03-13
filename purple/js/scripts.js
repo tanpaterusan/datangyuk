4a7c917d6d322351f4b51a8a54fae431f1954ed1
tanpaterusan <amaliatanpaterusan@gmail.com>
2024-03-13 08:59:54 +0700

    add white

--- purple/js/scripts.js
+++ purple/js/scripts.js
@@ -5,10 +5,12 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+/*url-name */
 // console.log('Window Location:', window.location);
 const too = window.location.search;
 const urlParams = new URLSearchParams(too);
 const param = urlParams.get('to');
 const upTo = param[0].toUpperCase() + param.substring(1);;
 // const upTo = param.capitalizeFirstLetter();
-document.getElementById('to').innerHTML = upTo +'  <i class="fa-solid fa-heart"></i>';
+document.getElementById('to').innerHTML = upTo + '  <i class="fa-solid fa-heart"></i>';
