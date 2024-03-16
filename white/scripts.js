f3f8861ecdfc56d0dd402ef317c587a4b41836ec
tanpaterusan <amaliatanpaterusan@gmail.com>
2024-03-16 17:47:20 +0700

    update white
    add rsvp, change audio, merapikan maps, dll.

--- white/scripts.js
+++ white/scripts.js
@@ -14,5 +14,89 @@ const param = urlParams.get('to');
 const upTo = param[0].toUpperCase() + param.substring(1);;
 // const upTo = param.capitalizeFirstLetter();
 document.getElementById('to').innerHTML = upTo + '  <i class="fa-solid fa-heart"></i>';
+document.querySelector('#nama').value = upTo;
+
+//simply countdown
+simplyCountdown(".simply-countdown", {
+      year: 2024, // required
+      month: 4, // required
+      day: 13, // required
+      hours: 8, // Default is 0 [0-23] integer
+      minutes: 0, // Default is 0 [0-59] integer
+      seconds: 0, // Default is 0 [0-59] integer
+      words: {
+        //words displayed into the countdown
+        days: { singular: " hari", plural: "hari" },
+        hours: { singular: " jam", plural: "jam" },
+        minutes: { singular: " menit", plural: "menit" },
+        seconds: { singular: " detik", plural: "detik" },
+      },
+    });
+
+// tombol buka + audio
+    const rootElement = document.querySelector(":root");
+    const hero = document.getElementById('hero');
+    const audioIconWrapper = document.querySelector('.audio-icon-wrapper');
+    const audioIcon = document.querySelector('.audio-icon');
+    const song = document.querySelector('#song');
+
+    function disableScroll() {
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+      window.onscroll = function () {
+        window.scrollTo(scrollTop, scrollLeft);
+      }
+      rootElement.style.scrollBehavior = 'auto';
+    }
+
+    function enableScroll() {
+      window.onscroll = function () {
+      }
+      rootElement.style.scrollBehavior = 'smooth';
+      playAudio();
+    }
+
+    function playAudio() {
+      // song.volume = 0.5;
+      audioIconWrapper.style.display = 'flex';
+      song.play();
+
+      isPlaying = 'true';
+    }
+
+    audioIconWrapper.onclick = function () {
+      if (isPlaying) {
+        song.pause();
+        audioIcon.classList.remove('bi-play-circle');
+        audioIcon.classList.add('bi-pause-circle');
+      } else {
+        song.play();
+        audioIcon.classList.add('bi-play-circle');
+        audioIcon.classList.remove('bi-pause-circle');
+      }
+
+      isPlaying = !isPlaying;
+    }
+
+    disableScroll();
+    
+
+// rsvp
+window.addEventListener("load", function () {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+        method: 'POST',
+        body: data,
+    })
+        .then(() => {
+        alert("Konfirmasi kehadiran terkirim!");
+        })
+    });
+});
 
 
